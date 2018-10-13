493d6c8ce8ac85b5f1304930fab79539ceb8a69e
Reka <rekapap28@gmail.com>
2018-10-13 13:18:21 +0100

    fix regex

--- spec/item_factory_spec.rb
+++ spec/item_factory_spec.rb
@@ -1,11 +1,36 @@
 require 'item_factory'
+require 'brie'
 
 describe ItemFactory do
-  describe 'extreme' do
-    subject { ItemFactory.new.create_item('eXtreme item', 0, 0) }
-    it "returns true if 'extreme' (not case sensitive) is in the title of the object" do
+  describe '#create_item' do
+    subject { ItemFactory.new.create_item('eXtreme itemes', 0, 0, Brie) }
+    it "creates the correct item's update requirement from the name" do
       expect(subject).to receive(:update_quality).twice
       subject.update
+      subject.update
+    end
+    it "creates the correct type" do
+      expect(subject).to be_an_instance_of(Brie)
+    end
+  end
+
+  describe '#extreme?' do
+    subject { ItemFactory.new }
+    it "returns true if 'extreme' (not case sensitive) is in the title of the object" do
+      expect(subject.extreme?('eXtreme name')).to eq true
+    end
+    it "returns false if 'extreme' (not case sensitive) is not in the title of the object" do
+      expect(subject.extreme?('name')).to eq false
+    end
+  end
+
+  describe '#even vowels?' do
+    subject { ItemFactory.new }
+    it "returns true if there are even vowels in the title of the object" do
+      expect(subject.even_vowels?('name')).to eq true
+    end
+    it "returns false if there are  not even vowels in the title of the object" do
+      expect(subject.even_vowels?('namei')).to eq false
     end
   end
 end
